6aff6cc5e01a70f49efbb8420b3d78fe9e59f5b3
Jared McBride <jared.mcbride@svu.edu>
2024-03-25 07:40:20 -0400

    Added composite newton cotes

--- NumCalc.jl
+++ NumCalc.jl
@@ -36,4 +36,42 @@ function newton_cotes(f, a, b; n = 5, closed = true)
             S = as/h)     # Quadrature scheme
 end
 
+"""
+Composite Newton-Cotes
+by Jared McBride (3-25-2024, Buena Vista, VA)
+
+Uses the function `newton_cotes` to form a composite quadrature rule. 
+
+inputs:
+    f - integrand,
+    a, b - left and right end points of he whole interval of integration,
+    num_blks - the number of subintervals,
+    n - degree of quadrature, as in we take n+1 points from each subinterval resulting in `n*num_blks + 1` total points,
+
+outputs:
+
+"""
+function composite_newton_cotes(f, 
+    a,                     # Left limit of integration
+    b; 
+    num_blks   = 25,       # Total number of points 
+    n          = 3,        # degree of quadrature
+    tot_points = num_blks * n - (num_blks -1), # Total number of points used
+    x          = a : (b - a) / (tot_points - 1) : b,
+    closed     = true
+)
+h = x[2] - x[1]
+S = zeros(tot_points)
+for m = 1:num_blks
+    S[(n-1)*(m-1)+1 : (n-1)*m+1] += 
+        nc.newton_cotes(f, x[(n-1)*(m-1)+1], x[(n-1)*m+1]; n = n-1, closed).S
+end
+
+x = a : (b - a) / (tot_points - 1) : b
+
+return (I = h*S'f.(x), # The accual quadrature
+        S = S,         # Quadrature scheme
+        x = x)
+end
+
 end # module
